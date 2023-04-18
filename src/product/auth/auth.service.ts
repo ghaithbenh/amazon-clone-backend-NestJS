55fe97a5ad6d62790ec085318f773fbe29160b80
unknown <ghaithallahbenhassine666.com>
2023-04-18 06:27:00 +0200

    first commit

---
+++ src/product/auth/auth.service.ts
@@ -0,0 +1,86 @@
+import { Injectable, HttpStatus, HttpException } from '@nestjs/common';
+
+import * as bcrypt from 'bcrypt';
+import { NewUserDTO } from 'src/user/dtos/new-user.dto';
+
+import { JwtService } from '@nestjs/jwt';
+import { ExistingUserDTO } from 'src/user/dtos/existing-user.dto';
+import { UserService } from 'src/user/user.service';
+import { UserDetails } from 'src/user/user-details.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 12);
+  }
+
+  async register(user: Readonly<NewUserDTO>): Promise<UserDetails | any> {
+    const { name, email, password } = user;
+
+    const existingUser = await this.userService.findByEmail(email);
+
+    if (existingUser)
+      throw new HttpException(
+        'An account with that email already exists!',
+        HttpStatus.CONFLICT,
+      );
+
+    const hashedPassword = await this.hashPassword(password);
+
+    const newUser = await this.userService.create(name, email, hashedPassword);
+    return this.userService._getUserDetails(newUser);
+  }
+
+  async doesPasswordMatch(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword);
+  }
+
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<UserDetails | null> {
+    const user = await this.userService.findByEmail(email);
+    const doesUserExist = !!user;
+
+    if (!doesUserExist) return null;
+
+    const doesPasswordMatch = await this.doesPasswordMatch(
+      password,
+      user.password,
+    );
+
+    if (!doesPasswordMatch) return null;
+
+    return this.userService._getUserDetails(user);
+  }
+
+  async login(
+    existingUser: ExistingUserDTO,
+  ): Promise<{ token: string } | null> {
+    const { email, password } = existingUser;
+    const user = await this.validateUser(email, password);
+
+    if (!user)
+      throw new HttpException('Credentials invalid!', HttpStatus.UNAUTHORIZED);
+
+    const jwt = await this.jwtService.signAsync({ user });
+    return { token: jwt };
+  }
+
+  async verifyJwt(jwt: string): Promise<{ exp: number }> {
+    try {
+      const { exp } = await this.jwtService.verifyAsync(jwt);
+      return { exp };
+    } catch (error) {
+      throw new HttpException('Invalid JWT', HttpStatus.UNAUTHORIZED);
+    }
+  }
+}
